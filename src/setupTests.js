b504e5eb3ae445b7709c3aba63fc5d0bbde5b82f
Damilola Faseun <fashfired@gmail.com>
2020-07-05 20:07:31 +0100

    Set up a test for the app

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,16 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+let savedItem = {};
+
+const localStorageMock = {
+  setItem: (key, item) => {
+    savedItem[key] = item;
+  },
+
+  getItem: (key) => savedItem[key],
+
+  clear:() => savedItem = {}
+};
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+// window.localStorage = localStorageMock;
