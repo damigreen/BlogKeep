7c0c874ce92f848788a1b8cde40c398785df8afb
Damilola Faseun <fashfired@gmail.com>
2020-07-10 21:43:09 +0100

    Added styles for login form layout

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+
+
+const tailLayout = {
+  wrapperCol: {
+    offset: 4,
+    span: 16,
+  },
+};
+
 
 const LoginForm = ({ username, password, handleLogin }) => {
+  const [user, setUser] = useState({
+    isChecked: false,
+    name: '',
+    username: '',
+  });
 
+  const onCheckClicked = e => {
+    setUser({
+      isChecked: e.target.checked,
+      name: window.localStorage.name,
+      username: window.localStorage.username,
+    });
+  }
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+  
   return (
-    <div className="login-page-center">
-      <h1 className='login-page-header'>log in to application</h1>
-      <form className='login-page-form' onSubmit={handleLogin}>
-        <div>
-          username
-          <input {...username} />
+    <div>
+      <div className="login-page-box" >
+          <h1 className='login-page-header'>SIGN IN</h1>
+          <Form
+            className='login-page-form'
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={handleLogin}
+            onFinishFailed={onFinishFailed}
+          >
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+              >
+              <Input {...username} />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+              >
+              <Input.Password {...password} />
+            </Form.Item>
+
+            <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+              <Checkbox user={user}>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button style={{backgroundColor: '#242b31', borderColor: '#242b31'}} type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>      
         </div>
-        <div>
-          password
-          <input {...password} />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </div>
+      </div>
   );
 };
 
